ff75cfa254a0898634303c6989521a6f05d9acad
Boscomunich <solomon4chidera@gmail.com>
2023-10-10 12:24:36 +0000

    file storage and init file

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Module for FileStorage autoinit."""
+
+import engine
+storage = FileStorage()
+storage.reload()
---
+++ models/engine/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""Module for FileStorage"""
+
+from engine.file_storage import FileStorage
