dcc669763752c66c8a9a0a4b0756d536e54f7e30
Boscomunich <solomon4chidera@gmail.com>
2023-10-12 05:48:40 +0000

    user class

---
+++ models/user.py
@@ -0,0 +1,10 @@
+#!usr/bin/python3
+"""user class module"""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+	"""class representing a user"""
+	email: str = ""
+	password: str = ""
+	first_name: str = ""
+	last_name: str = ""
